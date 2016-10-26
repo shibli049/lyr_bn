9c97eb71511bfb69f45ae603dc16a2decab6b926
shibli049 <shibli05@gmail.com>
2016-10-26 15:20:31 +0600

    jibonanondo das, sukumar roy and robi thakur as a poet

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, url_for, redirect,request
 
-from wikisource import sukumar_parser
+from wikisource import poet_parser
 from pymarkovchain import MarkovChain
+import logging
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+sukumar_roy_url = "https://goo.gl/Lcv5Vr"
+jibonanondo_das_url="https://goo.gl/0YYcuI"
+robi_thakur_url="https://goo.gl/vzuTbf"
 
 app = Flask(__name__)
 app.debug = True
@@ -18,22 +24,40 @@ def lyrics():
 
     if not artist:
         return redirect(url_for('index'))
+    artist = artist.lower()
 
-    lyrics = sukumar_parser.getPeoms(\
-        sukumar_parser.getHtmlFromUrl(\
-            sukumar_parser.sukumar_roy_url),\
-        size=size)
+    if('sukumar roy' == artist):
+        lyrics = poet_parser.getPeoms(\
+            poet_parser.getHtmlFromUrl(\
+                sukumar_roy_url),\
+            linkSelector="div.mw-category a",\
+            poemSelector = "div.poem center p",\
+            size=size)
+    elif('jibonanondo das' == artist):
+        lyrics = poet_parser.getPeoms(\
+            poet_parser.getHtmlFromUrl(\
+                jibonanondo_das_url),\
+            poemSelector = "div.poem p",\
+            size=size)
+    elif('robi thakur' == artist):
+        lyrics = poet_parser.getPeoms(\
+            poet_parser.getHtmlFromUrl(\
+                robi_thakur_url),\
+            poemSelector = "div.poem p",\
+            size=size)
+    else:
+        lyrics=""
 
     result = []
-    print(len(lyrics))
+    logging.info(len(lyrics))
     if(len(lyrics) > 0 and len(lyrics.strip()) > 0):
         mc = MarkovChain()
-        mc.generateDatabase(lyrics)
+        mc.generateDatabase(lyrics,sentenceSep='[।!?\n]' )
         for line in range(0, lines):
             result.append(mc.generateString())
     else:
         result = ["No poet found!"]
-    print(result)
+    logging.info(result)
     return render_template('lyrics.html', result=result, artist=artist)
 
 if __name__ == '__main__':
